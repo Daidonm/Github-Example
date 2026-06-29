34b6d0c4a5e7ba08ed7c9ac8f90c3a7c7629ab64
dai48 <phet.lue@gmail.com>
2026-06-29 11:39:16 +0700

    modified conflict_1

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-console.log("Hello, World!")
-<<<<<<< HEAD    
+console.log("Hello, World!")   
 console.log("Hello Github")
-=======
 console.log("Github")
->>>>>>> ecdad980f6a228068b8632137942f146ecaa8dfe
